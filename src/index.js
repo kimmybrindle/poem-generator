5ee18594784054fa41aecf1dde482a087bb31d62
kimmybrindle <kimmybrindle@gmail.com>
2025-10-07 18:09:06 +1100

    updated the ux and removed console.log

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 function displayPoem(response) {
-  console.log("Poem generated.");
   new Typewriter("#poem", {
     strings: response.data.answer,
     autoStart: true,
@@ -17,9 +16,9 @@ function generatePoem(event) {
   let prompt = `User instructions: Generate a poem about ${instructionInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  console.log("Generating poem...");
-  console.log(`Prompt: ${prompt}`);
-  console.log(`Context: ${context}`);
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⏳ Generating a poem about ${instructionInput.value}</div>`;
 
   axios.get(apiUrl).then(displayPoem);
 }
